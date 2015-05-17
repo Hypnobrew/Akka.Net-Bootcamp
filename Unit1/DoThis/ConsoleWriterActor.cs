ed078d862bfba4750cc67f364526b67fc8937371
Cristoffer Andersson <cristoffer.andersson@gmail.com>
2015-05-17 15:17:30 +0200

    1.1 - 1.3

---
+++ Unit1/DoThis/ConsoleWriterActor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Akka.Actor;
+
+namespace WinTail
+{
+    class ConsoleWriterActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            if (message is InputError)
+            {
+                var msg = message as InputError;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(msg.Reason);
+            }
+            else if (message is InputSuccess)
+            {
+                var msg = message as InputSuccess;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(msg.Reason);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
